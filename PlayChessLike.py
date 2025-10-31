bb91ca4dea20d048b7d9bca44e53879268feb79f
mhancherick <141765650+mhancherick@users.noreply.github.com>
2025-10-30 20:20:34 -0400

    Updated comments

--- PlayChessLike.py
+++ PlayChessLike.py
@@ -1,11 +1,8 @@
 from ChessLike import ChessLike
 from ChessLikeGUI import ChessLikeGUI
 
-# Create game instance
 game = ChessLike()
 
-# Create GUI wrapper
 gui = ChessLikeGUI(game)
 
-# Run the game
 gui.run_game()
